b5e5d080dc67ed066e400f4b94f737266f6ba979
abhivir-42 <abhivir42@gmail.com>
2025-02-21 02:05:34 +0000

    Feat: Create navigation header component

--- ad-generation-tool/src/components/ErrorMessage.tsx
+++ ad-generation-tool/src/components/ErrorMessage.tsx
@@ -1,1 +1,17 @@
- 
+'use client';
+
+import React from 'react';
+
+interface ErrorMessageProps {
+  message: string;
+}
+
+const ErrorMessage: React.FC<ErrorMessageProps> = ({ message }) => {
+  return (
+    <div className="mt-4 p-4 bg-red-500/20 border border-red-500 rounded-lg text-red-200">
+      {message}
+    </div>
+  );
+};
+
+export default ErrorMessage; 
