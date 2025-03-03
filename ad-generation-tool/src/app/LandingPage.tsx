940847b30ca7071f7e6b525661352bb0cdbcf8d3
abhivir-42 <abhivir42@gmail.com>
2025-03-03 19:55:33 +0000

    Fix: Remove unused imports - Removed unused Image import from next/image

--- ad-generation-tool/src/app/LandingPage.tsx
+++ ad-generation-tool/src/app/LandingPage.tsx
@@ -2,7 +2,6 @@
 
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
 // import AudioPlayer from '@/components/AudioPlayer';
 
 // const SAMPLE_AUDIO_FORMATS = {
